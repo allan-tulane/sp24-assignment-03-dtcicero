5f0f73390949c605a3062290579c8de61bf6aaed
dtcicero <130113355+dtcicero@users.noreply.github.com>
2024-04-17 00:41:03 +0000

    sp24-assignment-03-dtcicero

--- answers.md
+++ answers.md
@@ -1,8 +1,31 @@
 # CMPS 2200 Assignment 3
 ## Answers
 
-**Name:**_________________________
+**Name:** Daniel Cicero
 
 
 Place all written answers from `assignment-03.md` here for easier grading.
 
+1a. Start by selecting the highest denomination coin possible, 2^k, such that 2^k <= N. Then, subtract 2^k from N. Finally, repeat steps 1 and 2 until N = 0.
+
+1b. Greedy Choice: The greedy choice property holds that always choosing the locally optimal solution will necessarily lead to the overall optimal solution. In this case, at each step the greedy choice is made by choosing the highest denomination coin possible (maximizing the value of k) as to minimize the number of coins needed. 
+
+Optimal Substructure: This property holds that the optimal solution contains optimal solutions to contained subproblems. In this case, we can look at the remaining value N after choosing the highest denomination coin 2^k. Finding the optimal solution for this new N is equivalent so the optimal solution for the greater problem, proving its optimal substructure. 
+
+1c. Work: O(log n)
+    Span: O(1)
+
+
+2a. Suppose the available denominations in Fortuito are as follows: D1 = $1, D2 = $3, and D3 = $4. In this case, suppose N = 6. Using my algorithm above, we would choose D3 = $4 first, as it is the largest available value <= N. We would then be forced to choose D1 = $1 twice, for a total of 3 coins: {D3, D1, D1}. However, the optimal solution in Fortuito would be to simply choose D2 = $3 twice: {D2, D2}. Therefore, my solution above is not always optimal in Fortuito.
+
+2b. This problem exhibits optimal substructure for the same reason as does problem 1, which is that optimal solutions for subproblems within the greater problem can be constructed from the solution of the greater problem. For instance, in the process of solving the fewest coins for any N, we also contain in that solution the answer to fewest coins for any N', so long as 0 <= N' <= N. Therefore, optimal substructure is proven.
+
+2c. 1. Create a boolean array 'possible' of size N+1. Initialize all values to False.
+2. Set possible[0] to True, as it is always possible to make change for $0.
+3. For each denomination D:
+      For each amount i from 0 to N - D:
+        If possible[i] is True, set possible[i + D] to True
+   If possible[N] is True, it is possible to make change for N.
+
+   Work: O(N)
+   Span: O(N)
--- main.py
+++ main.py
@@ -7,23 +7,80 @@ test_cases = [('book', 'back'), ('kookaburra', 'kookybird'), ('elephant', 'relev
 alignments = [('b--ook', 'bac--k'), ('kook-ab-urr-a', 'kooky-bi-r-d-'), ('relev--ant','-ele-phant'), ('AAAGAATTCA', 'AAA---T-CA')]
 
 def MED(S, T):
-    # TO DO - modify to account for insertions, deletions and substitutions
-    if (S == ""):
-        return(len(T))
-    elif (T == ""):
-        return(len(S))
+  if S == "":
+      return len(T)  
+  elif T == "":
+      return len(S)  
+  else:
+      if S[0] == T[0]:
+          return MED(S[1:], T[1:]) 
+      else:
+          
+          insert = 1 + MED(S, T[1:])     
+          delete = 1 + MED(S[1:], T)     
+          substitute = 1 + MED(S[1:], T[1:]) 
+          return min(insert, delete, substitute)
+
+
+def fast_MED(S, T, memo={}):
+  if (S, T) in memo:
+      return memo[(S, T)]
+
+  if S == "":
+      result = len(T)
+  elif T == "":
+      result = len(S)
+  elif S[0] == T[0]:
+      result = fast_MED(S[1:], T[1:], memo)
+  else:
+      insert = 1 + fast_MED(S, T[1:], memo)
+      delete = 1 + fast_MED(S[1:], T, memo)
+      substitute = 1 + fast_MED(S[1:], T[1:], memo)
+      result = min(insert, delete, substitute)
+
+  memo[(S, T)] = result
+  return result
+
+def fast_align_MED(S, T):
+  m, n = len(S), len(T)
+
+
+  memo = [[0] * (n + 1) for _ in range(m + 1)]
+  
+
+  for i in range(m + 1):
+      memo[i][0] = i
+  for j in range(n + 1):
+     memo[0][j] = j
+
+  for i in range(1, m + 1):
+    for j in range(1, n + 1):
+      if S[i - 1] == T[j - 1]:
+         memo[i][j] = memo[i - 1][j - 1]
+      else:
+          memo[i][j] = min(memo[i - 1][j], memo[i][j - 1], memo[i - 1][j - 1]) + 1
+
+  align_S = ''
+  align_T = ''
+  i, j = m, n
+  while i > 0 or j > 0:
+    if i > 0 and j > 0 and S[i - 1] == T[j - 1]:
+        align_S = S[i - 1] + align_S
+        align_T = T[j - 1] + align_T
+        i -= 1
+        j -= 1
+    elif j > 0 and (i == 0 or memo[i][j - 1] + 1 == memo[i][j]):
+        align_S = '-' + align_S
+        align_T = T[j - 1] + align_T
+        j -= 1
+    elif i > 0 and (j == 0 or memo[i - 1][j] + 1 == memo[i][j]):
+        align_S = S[i - 1] + align_S
+        align_T = '-' + align_T
+        i -= 1
     else:
-        if (S[0] == T[0]):
-            return(MED(S[1:], T[1:]))
-        else:
-            return(1 + min(MED(S, T[1:]), MED(S[1:], T)))
+        align_S = S[i - 1] + align_S
+        align_T = T[j - 1] + align_T
+        i -= 1
+        j -= 1
 
-
-def fast_MED(S, T, MED={}):
-    # TODO -  implement top-down memoization
-    pass
-
-def fast_align_MED(S, T, MED={}):
-    # TODO - keep track of alignment
-    pass
-
+  return align_S, align_T
